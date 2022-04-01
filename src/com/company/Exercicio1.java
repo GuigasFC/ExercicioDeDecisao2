e022aa1083e406403762e2df935a28a7b9d687f7
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-04-01 17:22:48 -0300

    Init

--- src/com/company/Exercicio1.java
+++ src/com/company/Exercicio1.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Exercicio1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.print("Digite um número: ");
+        double numero1 = scanner.nextDouble();
+        System.out.print("Digite um outro número: ");
+        double numero2 = scanner.nextDouble();
+        Boolean numero1EMaiorOuMenor = numero1 > numero2;
+        if (numero1EMaiorOuMenor) {
+            System.out.println("O primeiro número é maior que o segundo.");
+        } else {
+            System.out.println("O segundo número é maior que o primeiro. ");
+        }
+        scanner.close();
     }
 }
